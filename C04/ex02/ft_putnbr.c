5d66b007e67909d6703fdd39445dd8a2dc3c00d6
Balint Groz <bgroz@c2r4p4.42vienna.com>
2024-03-07 10:04:10 +0100

    ex04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgroz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 12:06:04 by bgroz             #+#    #+#             */
+/*   Updated: 2024/02/28 17:09:54 by bgroz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else
+	{
+		if (nb < 0)
+		{
+			write(1, "-", 1);
+			nb = nb * -1;
+		}
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		ft_putnbr(atoi(argv[1]));
+		write(1, "\n", 1);
+	}
+	return (0);
+}
+*/
---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgroz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 17:47:06 by bgroz             #+#    #+#             */
+/*   Updated: 2024/02/29 09:27:55 by bgroz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		int	result = ft_atoi(argv[1]);
+		printf("%d\n", result);
+	}
+	return (0);
+}
+*/
